e250631a13f9f9064ef44cd95cb432e3022347da
Olivier Martin <olivier4.martin@orange.com>
2017-06-19 15:54:01 +0200

    add bg-multi-screen.sh
    Script that detects how many monitors are connected and display the
    background to the good one.
    
    This script is triggered by i3. It allows me to switch between my
    workplace and on-the-go setups.

---
+++ bg-multi-screen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONNECTED_DISPLAY=$(xrandr | cut -d' ' -f2 | grep '^connected' | wc -l)
+
+if [ $CONNECTED_DISPLAY -eq 1 ];
+  then
+    feh --scale-down --bg-scale "/usr/share/backgrounds/background.png"
+  else
+    feh --scale-down --xinerama-index 1 \
+      --bg-scale "/usr/share/backgrounds/background.png"
+fi
