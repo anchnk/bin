3b10f6e629456a45b938d8f90dd2f7db4796d5f6
Olivier Martin <olivier4.martin@orange.com>
2017-07-21 22:20:36 +0200

    remove background ampersand which aren't necessary

--- monitors-setup.sh
+++ monitors-setup.sh
@@ -2,13 +2,13 @@
 
 CONNECTED_MONITORS=`xrandr | grep " connected " | wc -l`
 
-$HOME/bin/set-bg.sh &
+$HOME/bin/set-bg.sh
 
 if [ $CONNECTED_MONITORS -gt 1 ];
 then
-  $HOME/.screenlayout/office-large-dual.sh &
+  $HOME/.screenlayout/office-large-dual.sh
 else
-  $HOME/.screenlayout/home-single.sh &
+  $HOME/.screenlayout/home-single.sh
 fi
 
 $HOME/bin/launch-polybar.sh
