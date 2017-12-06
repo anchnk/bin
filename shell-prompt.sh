b1a4a57169b1b2708bd7fa4c4873042f356355d0
anchnk <515506+anchnk@users.noreply.github.com>
2017-12-06 21:37:09 +0100

    shell-prompt: center select menu
    This patch updates the position of the menu so that it's centered in a
    fixed with terminal. User input mappings are fixed and should expand
    dynamically according to tmux sessions. Some more work needs to be done.

--- shell-prompt.sh
+++ shell-prompt.sh
@@ -6,29 +6,65 @@
 # tmux has-session -t _default || tmux new-session -s _default -d
 
 # present menu for user to choose which workspace to open
-PS3="
-Please choose your session: "
-options=($(tmux list-sessions -F "#S" 2>/dev/null) "New Session" "bash" "exit")
-echo "Available sessions"
-echo "------------------"
-echo " "
-COLUMNS=1
-select opt in "${options[@]}"
-do
-	case $opt in
-		"New Session")
-			read -p "Enter new session name: " SESSION_NAME
-			tmux new -s "$SESSION_NAME"
-			break;;
-		"bash")
-			bash --login
-			break;;
-    "exit")
-      exit
+
+function prompt_headline() {
+  local USER=get_current_user
+  echo "Welcome `whoami`," `date '+%a%e/%m/%y %k:%M'`
+}
+
+PROMPT_HEADLINE=$(prompt_headline)
+
+function draw_separator() {
+  PROMPT_HEADLINE_LENGTH=${#PROMPT_HEADLINE}
+  printf '%.s-' {1..35}
+}
+
+function draw_menu() {
+  local POS=0
+  local CURRENT_LINE=0
+  COLUMNS=$(tput cols)
+  LINES=$(tput lines)
+  X=$(( ( $LINES - 0 ) / 2 ))
+  Y=$(( ( $COLUMNS - 35 ) / 2 ))
+
+  options=($(tmux list-sessions -F "#S" 2>/dev/null) "Start a new tmux session" "Start a bash shell" "Exit")
+
+  tput clear
+  tput cup $X $Y
+  CURRENT_LINE=$X
+  prompt_headline
+  ((CURRENT_LINE++))
+
+  tput cup $CURRENT_LINE $Y
+  draw_separator
+  ((CURRENT_LINE++))
+
+  for ((i=0;i<${#options[@]};i++)); do
+    local DISPLAY_INDEX=$(( $i + 1 ))
+    tput cup $CURRENT_LINE $Y
+    echo "$DISPLAY_INDEX) ${options[$i]}"
+    ((CURRENT_LINE++))
+  done
+
+  tput cup $CURRENT_LINE $Y
+  draw_separator
+  ((CURRENT_LINE++))
+
+  tput cup $CURRENT_LINE $Y
+  read -p 'What would you like to start with ? ' opt
+  case $opt in
+    2)
+      read -p "Enter new session name: " SESSION_NAME
+      tmux new -s "$SESSION_NAME"
       break;;
-		*)
-			tmux attach-session -t $opt
-			break
-			;;
-	esac
-done
+    3)
+      tput clear
+      bash --login
+      ;;
+    4)
+      break
+      ;;
+  esac
+}
+
+draw_menu
