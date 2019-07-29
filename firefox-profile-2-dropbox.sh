6028d32f6c73311403b5969f6963e4aa7ca6b8c8
anchnk <515506+anchnk@users.noreply.github.com>
2019-07-29 11:26:17 +0200

    backup(firefox2dropbox): get firefox profile name dynamically
    This patch updates the shell script that save the default firefox
    profile to Dropbox. This script is triggered daily on my machine through
    a cron job. The way we extract the firefox profile name is kinda rigid
    atm: it parses the ini configuration file and expect a rigid structure.
    
    At any time if the format of the configuration change, it could break
    that script.

--- firefox-profile-2-dropbox.sh
+++ firefox-profile-2-dropbox.sh
@@ -1,21 +1,33 @@
 #! /usr/bin/env bash
 
+firefox_folder="$HOME/.mozilla/firefox/"
+
+get_firefox_profile_name() {
+  local firefox_profiles_ini="$firefox_folder/profiles.ini"
+  local firefox_profile_name
+
+  # Fetch firefox default profile name
+  firefox_profile_name=$(awk -F '=' 'NR==2,/Default/{ print $2 }' "$firefox_profiles_ini")
+  echo "$firefox_profile_name"
+}
+
 save_firefox_profile_to_dropbox() {
   local firefox_profile_path
   local today
   local archive_path
 
-  firefox_profile_path="$HOME/.mozilla/firefox/lzvmqt6n.default"
+  firefox_profile_path="$firefox_folder"$(get_firefox_profile_name)
   today=$(date +%d-%m-%Y)
   archive_directory="/tmp/$today"
   archive_path="$archive_directory/firefox-browser-profile.tar.bz2"
   encrypted_archive_path="$archive_path.gpg"
 
+  echo "$archive_path"
+  echo "$firefox_profile_path"
   mkdir -p "$archive_directory"
   tar -jcvf "$archive_path" "$firefox_profile_path"
   gpg -c "$archive_path"
   rclone copy "$encrypted_archive_path" dropbox:Backups/"$today"
-
   rm -r "$archive_directory"
 }
 
